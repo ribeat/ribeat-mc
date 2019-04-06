7a156c5d1eae02b007ed8af329d91a9b7ec480e9
Ionut Roberto Neamtu <ionut.neamtu286@gmail.com>
2019-04-06 11:19:30 +0300

    Project created

---
+++ ribeat-mc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ribeat-mc
+//
+//  Created by Neamțu Ionuț-Roberto on 4/6/19.
+//  Copyright © 2019 888 Funcional Monkeys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ribeat-mcTests/ribeat_mcTests.swift
@@ -0,0 +1,34 @@
+//
+//  ribeat_mcTests.swift
+//  ribeat-mcTests
+//
+//  Created by Neamțu Ionuț-Roberto on 4/6/19.
+//  Copyright © 2019 888 Funcional Monkeys. All rights reserved.
+//
+
+import XCTest
+@testable import ribeat_mc
+
+class ribeat_mcTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
