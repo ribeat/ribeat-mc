e2e921cf974898240f5a7f3048c341a2cbdec91a
Ionut Roberto Neamtu <ionut.neamtu286@gmail.com>
2019-04-07 12:00:52 +0300

    Request auto updates every 5 seconds

--- ribeat-mc/CollectionViewController.swift
+++ ribeat-mc/CollectionViewController.swift
@@ -10,12 +10,10 @@ import UIKit
 import Alamofire
 
 class CollectionViewController: UICollectionViewController {
-//    var data: [MemeModel] = [MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50))),
-//    MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50))),
-//    MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50))),
-//    MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50)))]
+
     var data: [MemeModel] = [MemeModel]()
     var orders: [OrderModel] = []
+    var updateInterval: Timer!
     
     func addOrder() {
         for subview in view.subviews {
@@ -48,23 +46,8 @@ class CollectionViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
         arrayOfOrders.append(myLabel)
         addOrder()
+        self.updateInterval = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
         
-        Alamofire.request("http://192.168.86.188:8080/api/v1/orders?orderReady=0").responseString { response in
-            
-            //print ("\(response.result.value)")
-            if let jsonData = response.result.value?.data(using: String.Encoding.utf8) {
-                do {
-                    self.orders = try JSONDecoder().decode([OrderModel].self, from: jsonData)
-                } catch {
-                    
-                }
-
-                for order in self.orders {
-                    self.data.append(MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50))))
-                }
-                self.collectionView.reloadData()
-            }
-        }
         
         collectionView.dataSource = self
         collectionView.delegate = self
@@ -77,6 +60,27 @@ class CollectionViewController: UICollectionViewController {
                                  numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
+    @objc func runTimedCode() {
+        Alamofire.request("http://192.168.86.188:8080/api/v1/orders?orderReady=0").responseString { response in
+            
+            //print ("\(response.result.value)")
+            if let jsonData = response.result.value?.data(using: String.Encoding.utf8) {
+                do {
+                    self.orders = try JSONDecoder().decode([OrderModel].self, from: jsonData)
+                    
+                } catch {
+                    
+                }
+                self.data.removeAll()
+                
+                for order in self.orders {
+                    self.data.append(MemeModel(table: UITableView(frame: CGRect(x: 10, y: 10, width: 100, height: 100)), button: UIButton(frame: CGRect(x: 10, y: 200, width: 200, height: 50))))
+                }
+                self.collectionView.reloadData()
+                
+            }
+        }
+    }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.data.remove(at: indexPath.row)
@@ -87,9 +91,12 @@ class CollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCell", for: indexPath) as! CollectionViewCell
         cell.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-        cell.buttonOutlet.backgroundColor = UIColor.blue
-        cell.buttonOutlet.setTitle("Order ready", for: .normal)
-        cell.buttonOutlet.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        if cell.buttonOutlet != nil {
+            cell.buttonOutlet.backgroundColor = UIColor.blue
+            cell.buttonOutlet.setTitle("Order ready", for: .normal)
+            cell.buttonOutlet.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        }
+        
 //        cell.buttonOutlet.tag = self.indexRow
         cell.configure(with: data[indexPath.row], forOrder: self.orders[indexPath.row])
         
